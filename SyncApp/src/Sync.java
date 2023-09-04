cf42518df2d5aa8d5cd5df4906088ff646ad4328
RichLeonC <r29leonc@gmail.com>
2023-09-04 15:25:35 -0600

    Prueba

--- SyncApp/src/Sync.java
+++ SyncApp/src/Sync.java
@@ -4,8 +4,11 @@
 //package com.mycompany.sync;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class Barrier {
 
@@ -18,13 +21,16 @@ class Barrier {
 
     public synchronized void esperar() throws InterruptedException {
         waitingThreads++;
+
         if (waitingThreads < totalThreads) {
             wait();
-        } else {
-            notifyAll();
-            waitingThreads = 0;
         }
-    }
+
+       else {
+            notifyAll();
+            waitingThreads = 0; 
+        } 
+   }
 }
 
 class Semaforo {
@@ -134,125 +140,133 @@ class SyncLib {
 
 }
 
-class MergeSort extends Thread {
-
-    public MergeSort(){}
-    public static void mergeSort(int[] arr, int inicio, int fin, SyncLib syncLib) {
-    if (inicio < fin) {
-        int medio = (inicio + fin) / 2;
-
-        // Dividir la lista en dos
-        mergeSort(arr, inicio, medio, syncLib);
-        mergeSort(arr, medio + 1, fin, syncLib);
-
-        // Fusionar las dos sublistas
-        merge(arr, inicio, medio, fin, syncLib);
-    }
-}
-
-    public static void parallelMergeSort(int[] arr, int inicio, int fin, SyncLib sync) {
-        if (inicio < fin) {
-            int medio = (inicio + fin) / 2;
-
-            Thread leftThread = new Thread(() -> {
-                parallelMergeSort(arr, inicio, medio, sync);
+class MergeSort{
+     
+      // Assuming system has 4 logical processors
+    private static final int MAX_THREADS = 4;
+       
+    // Custom Thread class with constructors
+    private static class SortThreads extends Thread{
+        SortThreads(Integer[] array, int begin, int end){
+            super(()->{
+                MergeSort.mergeSort(array, begin, end);
             });
-
-            Thread rightThread = new Thread(() -> {
-                parallelMergeSort(arr, medio + 1, fin, sync);
-            });
-
-            leftThread.start();
-            rightThread.start();
-
-            try {
-                leftThread.join();
-                rightThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            
-            //System.out.println("Llego a la llamada del merge");
-            
-            try {
-                sync.waitBarrera();
-                sync.decrementSemaforo();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } 
-           merge(arr, inicio, medio, fin,sync);
+            this.start();
         }
     }
-
-    private static void merge(int[] arr, int inicio, int medio, int fin, SyncLib sync) {
-      //  System.out.println("Entro al merge");
-        int n1 = medio - inicio + 1;
-        int n2 = fin - medio;
-
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-
-        for (int i = 0; i < n1; i++) {
-            left[i] = arr[inicio + i];
+     
+      // Perform Threaded merge sort
+    public static void threadedSort(Integer[] array){
+        long time = System.currentTimeMillis();
+        final int length = array.length;
+        boolean exact = length%MAX_THREADS == 0;
+        int maxlim = exact? length/MAX_THREADS: length/(MAX_THREADS-1);
+        // if workload is less and no more than 1 thread is required for work, then assign all to 1 thread
+        maxlim = maxlim < MAX_THREADS? MAX_THREADS : maxlim;
+        // To keep track of threads
+        final ArrayList<SortThreads> threads = new ArrayList<>();
+        // Since each thread is independent to work on its assigned chunk,
+        // spawn threads and assign their working index ranges
+        // ex: for 16 element list, t1 = 0-3, t2 = 4-7, t3 = 8-11, t4 = 12-15
+        for(int i=0; i < length; i+=maxlim){
+            int beg = i;
+            int remain = (length)-i;
+            int end = remain < maxlim? i+(remain-1): i+(maxlim-1); 
+            final SortThreads t = new SortThreads(array, beg, end);
+            // Add the thread references to join them later
+            threads.add(t);
         }
-        for (int i = 0; i < n2; i++) {
-            right[i] = arr[medio + 1 + i];
+        for(Thread t: threads){
+            try{
+                  // This implementation of merge requires, all chunks worked by threads to be sorted first.
+                // so we wait until all threads complete
+                t.join();
+            } catch(InterruptedException ignored){}
         }
-
-        int i = 0, j = 0;
-        int k = inicio;
-
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) {
-                arr[k] = left[i];
-                i++;
-            } else {
-                arr[k] = right[j];
-                j++;
-            }
-            k++;
+        // System.out.println("Merging k-parts array, where m number of parts are distinctly sorted by each Threads of available MAX_THREADS="+MAX_THREADS);
+        /*
+          The merge takes 2 parts at a time and merges them into 1,
+          then again merges the resultant into next part and so on...until end
+          For MAXLIMIT = 2 (2 elements per thread where total threads = 4, in a total of 4*2 = 8 elements)
+          list1 = (beg, mid); list2 = (mid+1, end);
+          1st merge = 0,0,1 (beg, mid, end)
+          2nd merge = 0,1,3 (beg, mid, end)
+          3rd merge = 0,3,5 (beg, mid, end)
+          4th merge = 0,5,7 (beg, mid, end)
+        */
+        for(int i=0; i < length; i+=maxlim){
+            int mid = i == 0? 0 : i-1;
+            int remain = (length)-i;
+            int end = remain < maxlim? i+(remain-1): i+(maxlim-1);
+            // System.out.println("Begin: "+0 + " Mid: "+ mid+ " End: "+ end + " MAXLIM = " + maxlim);
+            merge(array, 0, mid, end);
         }
-
-        while (i < n1) {
-            arr[k] = left[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2) {
-            arr[k] = right[j];
-            j++;
-            k++;
-        }
-
-        // Sincroniza antes de salir
-//        try {
-//            sync.waitBarrera();
-//            sync.decrementSemaforo();
-//            
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        time = System.currentTimeMillis() - time;
+        System.out.println("Time spent for custom multi-threaded recursive merge_sort(): "+ time+ "ms");
     }
-
+ 
+    // Typical recursive merge sort
+    public static void mergeSort(Integer[] array, int begin, int end){
+        if (begin<end){
+            int mid = (begin+end)/2;
+            mergeSort(array, begin, mid);
+            mergeSort(array, mid+1, end);
+            merge(array, begin, mid, end);
+        }
+    }
+    
+   
+     
+    //Typical 2-way merge
+    public static void merge(Integer[] array, int begin, int mid, int end){
+        Integer[] temp = new Integer[(end-begin)+1];
+         
+        int i = begin, j = mid+1;
+        int k = 0;
+ 
+        // Add elements from first half or second half based on whichever is lower,
+        // do until one of the list is exhausted and no more direct one-to-one comparison could be made
+        while(i<=mid && j<=end){
+            if (array[i] <= array[j]){
+                temp[k] = array[i];
+                i+=1;
+            }else{
+                temp[k] = array[j];
+                j+=1;
+            }
+            k+=1;
+        }
+ 
+        // Add remaining elements to temp array from first half that are left over
+        while(i<=mid){
+            temp[k] = array[i];
+            i+=1; k+=1;
+        }
+         
+        // Add remaining elements to temp array from second half that are left over
+        while(j<=end){
+            temp[k] = array[j];
+            j+=1; k+=1;
+        }
+ 
+        for(i=begin, k=0; i<=end; i++,k++){
+            array[i] = temp[k];
+        }
+    }
+    
 }
 
 public class Sync {
 
-    public static void main(String[] args) {
-        int []array = {5,3,8,2,7,4,1};
-        
-        int threads = Runtime.getRuntime().availableProcessors();
-        
-        SyncLib sync = new SyncLib(threads);
-        
+    public static void main(String[] args) throws InterruptedException {
+        Integer[] arr = {83, 86, 77, 15, 93, 35, 86, 92, 49, 21, 
+         62, 27, 90, 59, 63, 26, 40, 26, 72, 36};
+
+        SyncLib syncLib = new SyncLib(2); // Cambia el valor según la cantidad de hilos que deseas utilizar
         MergeSort merge = new MergeSort();
-        System.out.println("Antes del parallel");
-        merge.parallelMergeSort(array, 0, array.length-1, sync);
-        
-        System.out.println("Hola");
-        for (int i:array) {
-            System.out.println(i+" ");
-        }
+        MergeSort.threadedSort(arr);
+     
+
+        System.out.println("Arreglo ordenado: " + Arrays.toString(arr));
     }
 }
